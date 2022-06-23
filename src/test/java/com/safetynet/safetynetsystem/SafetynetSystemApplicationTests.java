4dddcb04a3c462209b3434c1425f4128b293f355
Anatoly <anatolyrytik@gmai.com>
2022-06-23 15:51:20 +0200

    initial commit

---
+++ src/test/java/com/safetynet/safetynetsystem/SafetynetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
