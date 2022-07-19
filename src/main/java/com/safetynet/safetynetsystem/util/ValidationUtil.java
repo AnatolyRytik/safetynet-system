d8ad1c6a649c9c68df5c469d36b18b97f8cb2abf
Anatoly <anatolyrytik@gmai.com>
2022-07-19 14:59:07 +0200

    add validation

---
+++ src/main/java/com/safetynet/safetynetsystem/util/ValidationUtil.java
@@ -0,0 +1,45 @@
+package com.safetynet.safetynetsystem.util;
+
+import com.safetynet.safetynetsystem.model.AbstractBaseEntity;
+import com.safetynet.safetynetsystem.util.error.IllegalRequestDataException;
+import com.safetynet.safetynetsystem.util.error.NotFoundException;
+
+public class ValidationUtil {
+    private ValidationUtil() {
+    }
+
+    public static <T> T checkNotFoundWithId(T object, long id) {
+        checkNotFoundWithId(object != null, id);
+        return object;
+    }
+
+    public static void checkNotFoundWithId(boolean found, long id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(AbstractBaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+
+    //  Conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.id() != id) {
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must has id=" + id);
+        }
+    }
+}
