903bda91411f4a2686f834e386fa440c8a8bdd34
Anatoly <anatolyrytik@gmai.com>
2022-07-19 14:57:22 +0200

    add db initializing process

--- src/main/java/com/safetynet/safetynetsystem/SafetynetSystemApplication.java
+++ src/main/java/com/safetynet/safetynetsystem/SafetynetSystemApplication.java
@@ -1,13 +1,66 @@
 package com.safetynet.safetynetsystem;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.safetynetsystem.model.Firestation;
+import com.safetynet.safetynetsystem.model.Medicalrecord;
+import com.safetynet.safetynetsystem.model.Person;
+import com.safetynet.safetynetsystem.repository.FirestationRepository;
+import com.safetynet.safetynetsystem.repository.MedicalRecordsRepository;
+import com.safetynet.safetynetsystem.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SafetynetSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SafetynetSystemApplication.class, args);
-	}
+    private JsonNode jsonTree;
+    private ObjectMapper mapper;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SafetynetSystemApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(PersonRepository personRepository, FirestationRepository firestationRepository, MedicalRecordsRepository medicalRecordsRepository) {
+        return args -> {
+            // read JSON and load json
+            mapper = new ObjectMapper();
+            TypeReference<Person> typeReference = new TypeReference<Person>() {
+            };
+            InputStream inputStream = TypeReference.class.getResourceAsStream("/json/data.json");
+            try {
+                jsonTree = mapper.readTree(inputStream);
+
+                List<Person> persons = getListFromJson("persons", Person.class);
+                personRepository.saveAll(persons);
+
+                List<Firestation> fireStations = getListFromJson("firestations", Firestation.class);
+                firestationRepository.saveAll(fireStations);
+
+                List<Medicalrecord> medicalRecords = getListFromJson("medicalrecords", Medicalrecord.class);
+                medicalRecordsRepository.saveAll(medicalRecords);
+
+                System.out.println("Data Saved!");
+            } catch (IOException e) {
+                System.out.println("Unable to save data: " + e.getMessage());
+            }
+        };
+    }
+
+    private <T> List<T> getListFromJson(String nodeName, Class<T> nodeClass) throws IOException {
+        List list = new ArrayList<T>();
+        for (JsonNode jsonNode : jsonTree.get(nodeName)) {
+            list.add(mapper.treeToValue(jsonNode, nodeClass));
+        }
+        return list;
+    }
 }
