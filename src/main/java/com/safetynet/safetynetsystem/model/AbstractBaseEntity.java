646424413397069287b88a9903da3fb5e160fae6
Anatoly <anatolyrytik@gmai.com>
2022-07-19 14:58:06 +0200

    add equals and hashcode

--- src/main/java/com/safetynet/safetynetsystem/model/AbstractBaseEntity.java
+++ src/main/java/com/safetynet/safetynetsystem/model/AbstractBaseEntity.java
@@ -2,32 +2,54 @@ package com.safetynet.safetynetsystem.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.util.ProxyUtils;
+import org.springframework.util.Assert;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class AbstractBaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
 
-    public Integer getId() {
+    // doesn't work for hibernate lazy proxy
+    public int id() {
+        Assert.notNull(id, "Entity must have id");
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     @JsonIgnore
     public boolean isNew() {
-        return this.id == null;
+        return id == null;
     }
 
+    //    https://stackoverflow.com/questions/1638723
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(ProxyUtils.getUserClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
 }
